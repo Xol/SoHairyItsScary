b78b9b5180578f4f10c35ef2cf46f4dbc9b0eb73
Xol <andre@andrerother.de>
2015-11-06 23:25:04 +0100

    Frist movement of the cylinder.

---
+++ SoHairyItsScary/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * 1000;
+        float moveVertical = Input.GetAxis("Vertical") * 1000;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        Debug.Log("Bewegung Hori: " + moveHorizontal.ToString());
+        Debug.Log("Bewegung Verti: " + moveVertical.ToString());
+        rb.AddForce(movement * speed);
+    }
+
+}
