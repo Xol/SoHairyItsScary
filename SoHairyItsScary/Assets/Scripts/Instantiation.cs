afb41d5793d13a130c1aaed0834ae552213b2851
Xol <andre@andrerother.de>
2015-11-07 12:15:08 +0100

    Dynamic generated floor.

---
+++ SoHairyItsScary/Assets/Scripts/Instantiation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instantiation : MonoBehaviour {
+    public Transform CubeForFloor;
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = -2; x < 6; x++)
+        {
+            for (int z = -2; z < 6; z++)
+            {
+                Instantiate(CubeForFloor, new Vector3(x, -0.5f, z), Quaternion.identity);
+            }
+        }
+    }
+}
