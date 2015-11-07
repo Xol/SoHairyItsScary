58f780c0364544a67e8fa293a19f05c3ed0a34ff
Xol <andre@andrerother.de>
2015-11-07 15:02:36 +0100

    Correction of the initial startzone.

--- SoHairyItsScary/Assets/Scripts/Instantiation.cs
+++ SoHairyItsScary/Assets/Scripts/Instantiation.cs
@@ -6,7 +6,7 @@ public class Instantiation : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        Instantiate(CubeForFloor, new Vector3(x, -0.5f, z), Quaternion.identity);
+        Instantiate(CubeForFloor, new Vector3(0, -0.5f, 0), Quaternion.identity);
         for (int x = -2; x < 6; x++)
         {
             for (int z = -2; z < 6; z++)
