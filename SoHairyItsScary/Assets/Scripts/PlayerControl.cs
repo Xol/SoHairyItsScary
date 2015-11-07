3127996b2878397ee798adb41a6743d2d5837efb
Xol <andre@andrerother.de>
2015-11-07 15:40:42 +0100

    Code organisation.

--- SoHairyItsScary/Assets/Scripts/PlayerControl.cs
+++ SoHairyItsScary/Assets/Scripts/PlayerControl.cs
@@ -16,41 +16,25 @@ public class PlayerControl : MonoBehaviour {
     {
         if (Input.GetButtonDown("Horizontal"))
         {
-            // moving to right
             if (Input.GetAxis("Horizontal") > 0)
             {
                 iTween.MoveBy(player, new Vector3(1, 0, 0), 0.2f);
                 mesh.transform.eulerAngles = new Vector3(0,90,0);
-                print("Winkel: " + mesh.transform.eulerAngles.y);
-                /*if(!FloatsAreEqual(mesh.transform.eulerAngles.y, 90f)) {
-                    mesh.transform.eulerAngles.Set(0, 90.0f, 0);
-                    print("Winkel: " + mesh.transform.eulerAngles.y);
-                }*/
-                //iTween.MoveBy(this.gameObject, new Vector3(1, 0, 0), 0.2f);
             }
-            // moving to left
             else if (Input.GetAxis("Horizontal") < 0)
             {
                 iTween.MoveBy(player, new Vector3(-1, 0, 0), 0.2f);
                 mesh.transform.eulerAngles = new Vector3(0, 270, 0);
-                print("Winkel: " + mesh.transform.eulerAngles.y);
-                /*if (!FloatsAreEqual(mesh.transform.eulerAngles.y, -90f))
-                {
-                    mesh.transform.eulerAngles.Set(0, -90.0f, 0);
-                    print("Winkel: " + mesh.transform.eulerAngles.y);
-                }*/
             }
         }
 
         if (Input.GetButtonDown("Vertical"))
         {
-            // moving to up
             if (Input.GetAxis("Vertical") > 0)
             {
                 iTween.MoveBy(player, new Vector3(0, 0, 1), 0.2f);
                 mesh.transform.eulerAngles = new Vector3(0, 0, 0);
             }
-            // moving to down
             else if (Input.GetAxis("Vertical") < 0)
             {
                 iTween.MoveBy(player, new Vector3(0, 0, -1), 0.2f);
